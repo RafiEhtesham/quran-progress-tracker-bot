427d602ebb6555b9d792b4d4e99fe2da8a29d619
RayBot <160085583+RafiEhtesham@users.noreply.github.com>
2024-03-16 15:01:34 +0600

    Update test.py

--- test.py
+++ test.py
@@ -1,11 +1,11 @@
-# import json
+import json
 
-# para = 4
-# quarter = 3
-# member = "1223"
+para = 4
+quarter = 3
+member = "1223"
 
-# with open('data.json', 'r+') as file:
-#     dic = json.load(file)
-#     dic[member] = [para, quarter]
-#     file.seek(0)
-#     json.dump(dic, file, indent= 4)
+with open('data.json', 'r+') as file:
+    dic = json.load(file)
+    dic[member] = [para, quarter]
+    file.seek(0)
+    json.dump(dic, file, indent= 4)
